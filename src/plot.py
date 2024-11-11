0eab6a55074b1f86238dde9023804fd24a3f3c32
thomjiji <vinyl.gambits.0n@icloud.com>
2024-11-11 18:21:54 +0800

    plot multiple colorspaces

--- src/plot.py
+++ src/plot.py
@@ -1,10 +1,25 @@
 from colour.colorimetry.datasets.illuminants import SDS_ILLUMINANTS
 import colour.plotting
 
-# colour.plotting.plot_RGB_colourspaces_in_chromaticity_diagram_CIE1931(
-#     ["ITU-R BT.2020", "ACEScg"]
-# )
 
+plot_kwargs = [
+    {"color": "r"},
+    {"color": "g"},
+    {"color": "b"},
+    {"marker": None},
+    {"linestyle": "dashed"},
+]
+
+colour.plotting.models.plot_RGB_colourspaces_in_chromaticity_diagram(
+    [
+        "ACES2065-1",
+        "ACEScg",
+        "ITU-R BT.2020",
+        "DaVinci Wide Gamut",
+        "FilmLight E-Gamut",
+    ],
+    plot_kwargs=plot_kwargs,
+)
 # colour.plotting.diagrams.plot_sds_in_chromaticity_diagram_CIE1931()
 
 # # Plot D65 in CIE1931
@@ -12,6 +27,6 @@ import colour.plotting
 # d60 = SDS_ILLUMINANTS["D60"]
 # colour.plotting.diagrams.plot_sds_in_chromaticity_diagram_CIE1931([d65, d60])
 
-colour.plotting.plot_ellipses_MacAdam1942_in_chromaticity_diagram_CIE1931()
-colour.plotting.plot_ellipses_MacAdam1942_in_chromaticity_diagram_CIE1960UCS()
-colour.plotting.plot_ellipses_MacAdam1942_in_chromaticity_diagram_CIE1976UCS()
+# colour.plotting.plot_ellipses_MacAdam1942_in_chromaticity_diagram_CIE1931()
+# colour.plotting.plot_ellipses_MacAdam1942_in_chromaticity_diagram_CIE1960UCS()
+# colour.plotting.plot_ellipses_MacAdam1942_in_chromaticity_diagram_CIE1976UCS()
